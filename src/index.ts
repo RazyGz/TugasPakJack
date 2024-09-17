d9ac840f8c516111481e13e8ae148b21338106f1
RazyGz <aaron_cahya_32rpl@student.smktelkom-mlg.sch.id>
2024-09-17 11:05:27 +0700

    add public file

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import Express from "express";
+import MedicineRoute from "./router/medicinerouter"
+
+const app = Express()
+// Izinkan body membaca request ke JSON Format
+
+app.use(Express.json())
+
+app.use(`/medicine`, MedicineRoute)
+
+
+const PORT = 1992
+app.listen(PORT, () => { })
+console.log(`Server Drugstore run on port ${PORT}`)
